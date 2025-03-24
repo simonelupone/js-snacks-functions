4354cc0193c2a0eb1ce813f8e9d618064d343df8
simonelupone <simone.lupone@gmail.com>
2025-03-24 15:54:15 +0100

    [feat]: add fourth exercise

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,19 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function countVowels(word) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (vowels.includes(word[i])) {
+            count++;
+        }
+    }
+    return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
